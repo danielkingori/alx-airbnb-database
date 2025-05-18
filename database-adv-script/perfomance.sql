b835e6b1ca18849be49ef37ef00e9043a76d0357
danielkingori <kingndiritu@gmail.com>
2025-05-18 20:22:47 +0300

    performance

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -6,6 +6,8 @@ FROM
     INNER JOIN Properties p ON b.PropertyID = p.PropertyID
     INNER JOIN Payments pm ON b.BookingID = pm.BookingID;
 
+WHERE b.BookingDate >= '2025-01-01' AND p.Location = 'Nairobi';
+
 EXPLAIN
 SELECT b.BookingID, b.BookingDate, u.UserID, u.Username, u.Email, p.PropertyID, p.PropertyName, p.Location, pm.PaymentID, pm.PaymentDate, pm.Amount, pm.PaymentMethod
 FROM
